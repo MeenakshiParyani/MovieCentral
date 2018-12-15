4d6702ed1bae22f7bf26a5d02b35d0391c286fb0
Viraj <u.viraj1995@gmail.com>
2018-12-15 06:03:04 -0800

    Updated PlayHistoryRepository

--- back-end/src/main/java/com/movie/central/MovieCentral/repository/PlayHistoryRepository.java
+++ back-end/src/main/java/com/movie/central/MovieCentral/repository/PlayHistoryRepository.java
@@ -22,7 +22,7 @@ public interface PlayHistoryRepository extends JpaRepository<PlayHistory, Long> 
     @Query(value = "select m.id, m.title, count(p.movie_id) as playcount from movie m  left join play_history p on m.id = p.movie_id group by m.id , m.title", nativeQuery = true)
     List<Object[]> getPlayPerMovie();
 
-    @Query(value = "select p.movie_id, m.title, count(p.movie_id) as playcount from play_history p, movie m where p.movie_id = m.id group by movie_id order by playcount limit 10", nativeQuery = true)
+    @Query(value = "select p.movie_id, m.title, count(p.movie_id) as playcount from play_history p, movie m where p.movie_id = m.id group by p.movie_id order by playcount desc limit 10", nativeQuery = true)
     List<Object[]> getMostPlayedMovies();
 
     @Query(value = "select p.customer_id, m.name, count(p.customer_id) as playcount from play_history p, customer m where p.customer_id = m.id group by customer_id order by playcount limit 10", nativeQuery = true)
